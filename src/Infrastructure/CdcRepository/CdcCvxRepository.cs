365087cc6f9233faf52cb3890065ae8103066451
yojana-hawaii <yojana.hawaii@gmail.com>
2024-12-16 15:56:09 -1000

    basic Get implementation of interfaces

---
+++ src/Infrastructure/CdcRepository/CdcCvxRepository.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Infrastructure.CdcRepository
+{
+    public class CdcCvxRepository : ICdcCvx
+    {
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// 
+
+        private readonly InventoryDbContext _dbContext;
+
+        public CdcCvxRepository(InventoryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<CdcCvx> FetchAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CdcCvx> GetAll()
+        {
+            return _dbContext.CdcCvxes;
+        }
+
+        public CdcCvx GetByCvxCode(string cvxCode)
+        {
+            if (string.IsNullOrWhiteSpace(cvxCode)) { throw new ArgumentNullException(nameof(cvxCode)); }
+            var _cdcCvx = _dbContext.CdcCvxes.FirstOrDefault(c => c.CdcCvxCode == cvxCode);
+            if (_cdcCvx == null) { throw new NullReferenceException(nameof(cvxCode)); }
+            return _cdcCvx;
+        }
+    }
+}
---
+++ src/Infrastructure/CdcRepository/CdcLookupNdcRepository.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Infrastructure.CdcRepository
+{
+    public class CdcLookupNdcRepository : ICdcLookupNdc
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// 
+
+        private readonly InventoryDbContext _dbContext;
+        public CdcLookupNdcRepository(InventoryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IEnumerable<CdcLookupNdc> FetchAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CdcLookupNdc> GetAll()
+        {
+            return _dbContext.CdcLookupNdcs;
+        }
+
+        public CdcLookupNdc GetByCvxCode(string cvxCode)
+        {
+            if (string.IsNullOrWhiteSpace(cvxCode)) { throw new ArgumentNullException(nameof(cvxCode)); }
+            var _ndc = _dbContext.CdcLookupNdcs.FirstOrDefault( n => n.CdcCvxCode == cvxCode );
+            if (_ndc == null) { throw new NullReferenceException(nameof(cvxCode)); }
+            return _ndc;
+        }
+    }
+}
