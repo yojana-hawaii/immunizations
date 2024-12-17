a6e67bd1307add671f5d9af6bb4e0882290e43b4
yojana-hawaii <yojana.hawaii@gmail.com>
2024-12-16 15:52:55 -1000

    update namespace for domain models

--- src/Domain/Model/VaccineSources.cs
+++ src/Domain/Model/VaccineSources.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Domain.domain
+namespace Domain.Model
 {
     public class VaccineSources
     {
